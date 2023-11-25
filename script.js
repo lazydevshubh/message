846921deccdcc8fa34708621587c5492035f8f78
lazydevshubh <42832244+lazydevshubh@users.noreply.github.com>
2023-11-25 17:58:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const wrapper = document.querySelector(".wrapper");
-const question = document.querySelector(".question");
+let question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
+const msgList = [
+  'ban jao na please 🥺',
+  'Pllllleeeeeaaaassseeee',
+  'accha to hu me itna 😉',
+  'jab tak yes nahi karoge me to nahi jaa raha 😏',
+  'kar bhi do abb, maan bhi jao 😏'
+] 
+
+let currentIndex = 0;
+
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you soon!";
+  question.innerHTML = "I knew you love me a lot 😍";
   gif.src =
     "https://media.giphy.com/media/MBaRIkFJiJ1yF5ZUy8/giphy-downsized.gif";
 });
@@ -15,6 +25,9 @@ noBtn.addEventListener("mouseover", () => {
   const maxX = window.innerWidth - noBtnRect.width;
   const maxY = window.innerHeight - noBtnRect.height;
 
+  question.innerHTML = msgList[currentIndex];
+  currentIndex += 1;
+  currentIndex = currentIndex % msgList.length;
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
 
